9105d83e41e8e4c810236c14320ffb3b4cc6c400
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-11-21 06:28:59 +0100

    16er commit

--- components/SessionProvider.js
+++
@@ -1,7 +0,0 @@
-import { SessionProvider } from "next-auth/react";
-
-function NextAuthProvider ({ children }) {
-  return <SessionProvider basePath="/api/auth">
-        {children}
-     </SessionProvider>;
-}
